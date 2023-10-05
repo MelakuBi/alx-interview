db7f447245f3bc3642de4a49a5cbac59ea7cbe6c
Melaku Birhanu <melakubirhanu@gmail.com>
2023-10-05 17:53:01 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-from typing import List
+
 def pascal_triangle(n):
-    ''' pascal triangle '''
-    triangle = []
+    ''' pascal list '''
+    list = []
     if type(n) is not int or n <= 0:
-        return triangle
+        return list
     for i in range(n):
         line = []
         for j in range(i + 1):
             if j == 0 or j == i:
                 line.append(1)
             elif i > 0 and j > 0:
-                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        triangle.append(line)
-    return triangle
+                line.append(list[i - 1][j - 1] + list[i - 1][j])
+        list.append(line)
+    return list
